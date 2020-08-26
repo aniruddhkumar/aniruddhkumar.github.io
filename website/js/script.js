c31f9dba8d83a5691a39779aab1cb5766bfc9570
Aniruddh Kumar <aniruddhkumar@users.github.com>
2020-08-26 10:11:37 +0530

    New website

---
+++ website/js/script.js
@@ -0,0 +1,11 @@
+/* JAVASCRIPT FILE */
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
